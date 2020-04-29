0db68f0715bccea7c0a2a15358968cab64e177d7
sranasinghe <sgkranasinghe96@gmail.com>
2020-04-29 09:31:19 +0530

    commit

---
+++ app/src/main/java/com/example/shopcenter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.shopcenter;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/shopcenter/madproject/Prevelent.java
@@ -0,0 +1,8 @@
+package com.example.shopcenter.madproject;
+
+class Prevelent {
+    public static class Currentcategories {
+        public static void getName() {
+        }
+    }
+}
